1be9f0600b42a5a749627aef1c1e19c4aad3fa3c
Geovana Augusta <geovanaag@hotmail.com>
2022-02-02 10:27:39 -0300

    Adição exercício 1 do dia 4.1

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/exerc1.js
@@ -0,0 +1,10 @@
+const myName= "Geovana";
+const birthCity= "Belo Horizonte";
+const birthYear= 1995;
+
+birthCity = 2030;
+birthCity = "Juiz de Fora";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
