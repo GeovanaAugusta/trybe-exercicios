c313e93ad54436ffad23281d7650fa194dba7012
Geovana Augusta <geovanaag@hotmail.com>
2022-02-02 10:47:41 -0300

    Adição exercício 3, melhorias 1, README com enunciado

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/exerc3.js
@@ -0,0 +1,13 @@
+const notaCandidata = 80;
+
+if (notaCandidata >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+}
+
+else if (notaCandidata < 80 && notaCandidata >= 60) {
+    console.log("Você está na nossa lista de espera")
+    }
+
+    else {
+        console.log("Você foi reprovada(o)")
+    }
