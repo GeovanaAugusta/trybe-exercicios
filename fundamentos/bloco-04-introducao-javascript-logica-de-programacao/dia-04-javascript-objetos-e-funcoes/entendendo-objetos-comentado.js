f945f8db8c2ec6c9a4b8154b1d524072f019054b
Geovana Augusta <geovanaag@hotmail.com>
2022-02-06 16:39:18 -0300

    Comentando para entender objetos

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/entendendo-objetos-comentado.js
@@ -0,0 +1,31 @@
+let conta = {
+    agencia: '0000',
+    banco: {
+      cod: '012',
+      id: 4,
+      nome: 'TrybeBank',
+    },
+  };
+
+// Criar uma variável let que pegue um objeto dentro do objeto e armazene todas as info de forma a se tornar um array que deve ser considerado na hora de acessar.
+    let infoDoBanco = 'banco';
+
+// Acesso ao array criado deve ser nomeLetObjeto[nomeLetArray] para acesso à tudo. Para acesso a algo específico nomeLetObjeto[nomeLetArray]['nome da propriedade do objeto']
+
+  console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+  console.log(conta[infoDoBanco]['nome']); // TrybeBank
+  
+// Existem n maneiras para o acesso às propriedades para o objeto, podendo ser:
+// nomeLetObjeto.propriedade no primeiro caso
+// nomeLetObjeto['propriedade'] no segundo caso
+
+  console.log(conta.agencia); // 0000
+  console.log(conta['agencia']); // 0000
+  
+// // Existem n maneiras para o acesso às propriedades de um objeto dentro de outro objeto, podendo ser:
+// nomeLetObjeto.propriedadeDoOutroObjeto.propriedadeDentroDoOuytoObjeto
+// nomeLetObjeto['propriedadeDoOutroObjeto']['propriedadeDentroDoOuytoObjeto']
+
+  console.log(conta.banco.cod); // 012
+  console.log(conta['banco']['id']); // 4
+
