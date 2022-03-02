e1443d36af50aab3a1fde30c88d6f3db9cfd6396
Geovana Augusta <geovanaag@hotmail.com>
2022-03-02 13:27:36 -0300

    Fixar dia 7.1

---
+++ fundamentos/bloco-07-introduçao-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/ternary-operator.js
@@ -0,0 +1,13 @@
+// A sintaxe básica do operador ternário é muito simples:
+`expressão verdadeira ou falsa` ? `retorno se verdadeira` : `retorno se falsa`;
+
+// Assim, por exemplo, podemos ter expressões com a seguinte estrutura:
+const trueExpression = (1 + 1 === 2) ? `isso é verdade` : `isso é mentira`;
+console.log(trueExpression); // isso é verdade
+
+const falseExpression = (2 + 2 === 3) ? `isso é verdade` : `isso é mentira`;
+console.log(falseExpression); // isso é mentira
+
+// O x é uma condição que será avaliada como verdadeira ou falsa; ?
+// O y é o retorno se a condição for verdadeira;
+// O z é o retorno se a condição for falsa;
