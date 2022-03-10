d1029fc36d800eb890231fc43085dd570a9aaae0
Geovana Augusta <geovanaag@hotmail.com>
2022-03-10 19:26:38 -0300

    Exercícios 8.2

---
+++ fundamentos/bloco-08-higher-order-functions-do-es6/dia-02-js-es6-higher-order-functions-foreach-find-some-every-sort/foreach-I.js
@@ -0,0 +1,31 @@
+// forEach 
+// Tudo o que você precisa fazer é chamar a função forEach no array e passar para ela a função que você criou. E mais nada! Esse é o poder da função forEach . 
+
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+
+function verifyGrades() {
+  students.forEach((student, index) => {
+    if (student.grade >= 60) {
+      students[index].approved = 'Aprovado';
+    } else {
+      students[index].approved = 'Recuperação';
+    }
+  });
+}
+
+verifyGrades();
+
+console.log(students);
+
+//  Array.find 
+// Ponto de observação: note que o parâmetro passado para numbers.find(), number => number % 5 == 0 , é uma arrow function, igual no primeiro exemplo de forEach, em que foi passada a função verifyGrade .
+
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+console.log(firstMultipleOf5);
---
+++ fundamentos/bloco-08-higher-order-functions-do-es6/dia-02-js-es6-higher-order-functions-foreach-find-some-every-sort/some-very.js
@@ -0,0 +1,23 @@
+// O exemplo abaixo usa o some para verificar se possui algum nome que começa com a letra desejada:
+
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); 
+console.log(verifyFirstLetter('x', listNames)); 
+
+// O exemplo abaixo usará o every para verificar se o estudante passou em todas as matérias:
+// Observe que foi usado Object.values junto com o every . Como o Object.values retorna um array apenas com os valores do objeto, o every percorrerá esse array retornado. Interessante essa combinação de funções, hein?! Experimente alguma combinação dessas na sua própria máquina agora!
+
+const grades = {
+    portugues: 'Aprovado',
+    matematica: 'Reprovado',
+    ingles: 'Aprovado',
+  };
+  
+  const verifyGrades = (studentGrades) => (
+    Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+  );
+  
+  console.log(verifyGrades(grades));
