45ea0225a3565786936914a0a357e67ea1cba372
Geovana Augusta <geovanaag@hotmail.com>
2022-03-12 18:01:29 -0300

    8.4

---
+++ fundamentos/bloco-08-higher-order-functions-do-es6/dia-04-higher-order-functions-reduce/reduce-I.js
@@ -0,0 +1,11 @@
+// O reduce possui uma outra diferença: você pode passar um segundo parâmetro para o reduce, logo após a função . Veja a seguir, será usado o último exemplo dado da soma dos números
+
+const numbers = [32, 15, 3, 2, -5, 56, 10];
+
+const getSum = (result, number) => {
+  console.log(result); // 32 47 50 52 47 103
+  return result + number;
+};
+
+const sumNumbers = numbers.reduce(getSum);
+console.log(sumNumbers); // 113
