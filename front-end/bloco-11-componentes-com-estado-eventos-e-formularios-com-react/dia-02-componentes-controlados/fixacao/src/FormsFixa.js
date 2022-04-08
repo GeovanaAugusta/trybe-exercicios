9f5f7b298ae71d13f5092a72aa75f14db6a47271
Geovana Augusta <geovanaag@hotmail.com>
2022-04-08 12:46:24 -0300

    Dia 11.2

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-componentes-controlados/fixacao/src/FormsFixa.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+class FormFixa extends React.Component {
+  constructor() {
+    super()
+
+    this.handleChange1 = this.handleChange1.bind(this);
+
+    this.state = {
+      nome: '',
+    }
+  }
+
+handleChange1(event) {
+  this.setState({
+    nome: event.target.value,
+  })
+}
+
+  render() {
+    return (
+    <div>
+      <label> 
+      <select>
+      <option value="option">Option</option>
+      <option value="option2">Option2</option>
+      <option value="option3">Option3</option>
+      </select>
+      </label>
+
+      <label>
+      Nome
+      <textarea 
+      onChange={ this.handleChange1 }
+      name= 'nome'
+      value={this.state.nome}
+      />
+      </label>
+
+      <label> 
+      Email
+      <input>
+
+      </input>
+      </label>
+
+      <label> 
+        Oi
+      <textarea>
+        Digite algo
+      </textarea>
+      </label>
+
+    </div>
+    )
+  }
+
+}
+
+export default FormFixa;
+
+// Para fixar
+// 1 - Crie um formulário com um campo select , dois inputs de tipo diferente, uma textarea e faça de um deles um componente controlado, ou seja, elementos do formulário controlados pelo Estado
+// 2 - Baixe a React Developer Tools e veja, nela, o dado inserido no elemento controlado sendo salvo no Estado.
