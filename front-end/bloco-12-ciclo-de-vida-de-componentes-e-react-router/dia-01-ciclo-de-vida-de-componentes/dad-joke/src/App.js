f99fc872ae2cfe07f543c3f7f3d0e2de27dd5052
Geovana Augusta <geovanaag@hotmail.com>
2022-04-13 22:02:47 -0300

    Dia 12.1

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/dad-joke/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DadJoke from './components/DadJoke';
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+  return (
+    <div className="App">
+      <DadJoke />    
+    </div>
+  );
+  }
+}
+
+export default App;
