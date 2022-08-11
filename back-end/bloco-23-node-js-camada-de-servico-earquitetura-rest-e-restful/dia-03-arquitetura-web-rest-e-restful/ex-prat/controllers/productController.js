8911620273534bde88c65ad3c49b48b58dabaf6c
Geovana Augusta <geovanaag@hotmail.com>
2022-08-10 21:36:04 -0300

    bloco 23 - dia 3

--- back-end/bloco-23-node-js-camada-de-servico-earquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/ex-prat/controllers/productController.js
+++ back-end/bloco-23-node-js-camada-de-servico-earquitetura-rest-e-restful/dia-03-arquitetura-web-rest-e-restful/ex-prat/controllers/productController.js
@@ -1,50 +1,13 @@
-// const express = require('express');
-// const ProductModel = require('../models/productModel');
+const express = require('express');
+const ProductModel = require('../models/productModel');
 
-// const router = express.Router();
+const router = express.Router();
 
 // router.get('/list-products', async (req, res) => {
 //   const products = await ProductModel.getAll();
 
 //   res.send(products);
 // });
-
-// router.get('/get-by-id/:id', async (req, res) => {
-//   const product = await ProductModel.getById(req.params.id);
-
-//   res.send(product);
-// });
-
-// router.post('/add-product', async (req, res) => {
-//   const { name, brand } = req.body;
-
-//   const newProduct = await ProductModel.add(name, brand);
-
-//   res.send(newProduct);
-// });
-
-// router.post('/delete-product/:id', async (req, res) => {
-//   const products = await ProductModel.exclude(req.params.id);
-
-//   res.send(products);
-// });
-
-// router.post('/update-product/:id', async (req, res) => {
-//   const { name, brand } = req.body;
-
-//   const products = await ProductModel.update(req.params.id, name, brand);
-
-//   res.send(products);
-// });
-
-// module.exports = router;
-
-// Refatorado 
-
-const express = require('express');
-const ProductModel = require('../models/productModel');
-
-const router = express.Router();
 
 // Removemos 'list-products' do endpoint pois uma requisição do tipo GET,
 // sem parâmetros que especifiquem um recurso (ex: id de um produto),
@@ -56,6 +19,14 @@ router.get('/', async (_req, res) => {
   // com sucesso e padronizamos o retorno para json.
   res.status(200).json(products);
 });
+
+
+// router.get('/get-by-id/:id', async (req, res) => {
+//   const product = await ProductModel.getById(req.params.id);
+
+//   res.send(product);
+// });
+
 
 // Removemos 'get-by-id' do endpoint pois uma requisição do tipo GET,
 // com parâmetros que especifiquem um recurso (ex: id de um produto),
@@ -74,24 +45,13 @@ router.get('/:id', async (req, res) => {
   res.status(200).json(product);
 });
 
-// Remova 'add-product' do endpoint pois uma requisição do tipo POST
-// já indica a criação de um novo recurso.
-router.post('/', async (req, res) => {
-  const { name, brand } = req.body;
+// router.post('/add-product', async (req, res) => {
+//   const { name, brand } = req.body;
 
-  if (name === undefined || brand === undefined) {
-    // Caso os dados do produto não sejam informados na requisição, utilize o
-    // status code 400 para indicar que a requisição não foi feita corretamente
-    // e padronize o retorno para json.
-    return res.status(400).json({ message: 'Informações do produto inválidas' });
-  }
+//   const newProduct = await ProductModel.add(name, brand);
 
-  const newProduct = await ProductModel.add(name, brand);
-
-  // Utilize o status code 201 para indicar que o produto foi criado, concluindo
-  // a requisição com sucesso, e padronize o retorno para json.
-  res.status(201).json(newProduct);
-});
+//   res.send(newProduct);
+// });
 
 // Altere o verbo HTTP para DELETE e remova 'delete-product' do endpoint,
 // uma vez que uma requisição do tipo DELETE já indica que o endpoint será
@@ -112,6 +72,12 @@ router.delete('/:id', async (req, res) => {
   // Além disso, padronize o retorno para json.
   res.status(204).json();
 });
+
+// router.post('/delete-product/:id', async (req, res) => {
+//   const products = await ProductModel.exclude(req.params.id);
+
+//   res.send(products);
+// });
 
 // Altere o verbo HTTP para PUT e remova 'update-product' do endpoint,
 // uma vez que uma requisição do tipo PUT já indica que o endpoint será
@@ -140,4 +106,44 @@ router.put('/:id', async (req, res) => {
   res.status(200).json({ name, brand });
 });
 
+// router.post('/update-product/:id', async (req, res) => {
+//   const { name, brand } = req.body;
+
+//   const products = await ProductModel.update(req.params.id, name, brand);
+
+//   res.send(products);
+// });
+
+
+
+
+
+
 module.exports = router;
+
+
+
+
+
+// Remova 'add-product' do endpoint pois uma requisição do tipo POST
+// já indica a criação de um novo recurso.
+router.post('/', async (req, res) => {
+  const { name, brand } = req.body;
+
+  if (name === undefined || brand === undefined) {
+    // Caso os dados do produto não sejam informados na requisição, utilize o
+    // status code 400 para indicar que a requisição não foi feita corretamente
+    // e padronize o retorno para json.
+    return res.status(400).json({ message: 'Informações do produto inválidas' });
+  }
+
+  const newProduct = await ProductModel.add(name, brand);
+
+  // Utilize o status code 201 para indicar que o produto foi criado, concluindo
+  // a requisição com sucesso, e padronize o retorno para json.
+  res.status(201).json(newProduct);
+});
+
+
+
+
